7978b399bc545d0a0e635f251dfb7888c823854a
kiptubei <kiptubei@gmail.com>
2020-11-06 17:55:37 +0300

    Added dic and prod configs,webpack dev server

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,24 @@
+
 const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html"
-    }),
+    })
   ],
   module: {
     rules: [
       {
-        test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader" //1. Turns sass into css
+        ]
+      }
+    ]
+  }
 };
