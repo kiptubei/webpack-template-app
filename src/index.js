abc00cfcd936b2cf84da471561b42f8622c761db
kiptubei <kiptubei@gmail.com>
2020-11-06 15:25:59 +0300

    Modified webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { run } from "./app/app";
-import { AlertService } from "./app/alert.service";
+import { run } from './app/app';
+import { AlertService } from './app/alert.service';
 import { ComponentService } from './app/component.service';
 
 const alertService = new AlertService();
